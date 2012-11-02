70e13cfe93de27c184891e205444fa6eaf06b28b
aquarla <satosamu@gmail.com>
2012-11-02 15:59:27 +0900

    改行を含む文字列のsudden_deathに対応

--- lib/sudden_death/sudden_death.rb
+++ lib/sudden_death/sudden_death.rb
@@ -2,15 +2,24 @@
 
 module SuddenDeath
   def self.sudden_death(text)
-    hankaku_count = text.split(//).count {|c| c.ascii_only? }
-    text_width = hankaku_count + (text.size - hankaku_count) * 2
-    top = "人" * (text_width / 2)
-    bottom = "Ｙ" * (text_width / 2)
+    lines = text.each_line.to_a.collect{|line| line.chomp }
+    max_width = lines.collect{|line| text_width(line)}.max
+
+    top = "人" * (max_width / 2)
+    middle = lines.collect{|line|
+      "＞　#{line + (" " * (max_width - text_width(line)))}　＜"
+    }.join("\n")
+    bottom = "Ｙ" * (max_width / 2)
 
     <<"SUDDENDEATH"
 ＿人#{top   }人＿
-＞　#{text  }　＜
+#{middle}
 ￣Ｙ#{bottom}Ｙ￣
 SUDDENDEATH
+  end
+
+  def self.text_width(line)
+    hankaku_count = line.split(//).count {|c| c.ascii_only? }
+    hankaku_count + (line.size - hankaku_count) * 2
   end
 end
